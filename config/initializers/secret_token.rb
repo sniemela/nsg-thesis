4498df4d4c6d32879d58ff19b0427a986224966d
Simo Niemelä <simo.niemela@gmail.com>
2011-08-07 19:09:09 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NsgThesis::Application.config.secret_token = 'f58e4ab5d73f09f6e18159ad99cbf61ab66a41096d58dfd9f5fee98991cb072334e7d2080989d112b1fb4fb4581858dd113f32a75eb04408f67728b3df474602'
