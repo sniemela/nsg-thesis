d5241b4cb2ecaf2a416cfc55babf7eadd0208dc3
Simo Niemelä <simo.niemela@gmail.com>
2011-09-05 12:15:20 +0300

    Add dummy layout and events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 NsgThesis::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  resources :events
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
