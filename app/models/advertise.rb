a4eaeed1e86764fda28ad81b12387bf4cba6dcb2
Simo Niemelä <simo.niemela@gmail.com>
2011-09-09 14:12:27 +0300

    Calculate price for an ad

--- app/models/advertise.rb
+++ app/models/advertise.rb
@@ -1,5 +1,22 @@
 class Advertise < ActiveRecord::Base
   belongs_to :client
 
+  has_many :taggings, :as => :taggable
+  has_many :tags, :through => :taggings
+
   has_attached_file :media
+
+  before_save :set_price
+
+  def set_price
+    self[:price] = calculate_price
+  end
+
+  def calculate_price
+    (tags.size * 5) + (duration_in_days * 5).round
+  end
+
+  def duration_in_days
+    (end_time - start_time) / 24 / 60 / 60
+  end
 end
