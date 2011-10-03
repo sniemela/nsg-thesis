81b0a22cdd66b478fd42bd834c9d064c2d792e8f
Simo Niemelä <simo.niemela@gmail.com>
2011-10-03 12:59:16 +0300

    Add error message box styles

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -28,11 +28,16 @@ class AdsController < ApplicationController
   def create
     ad_days = params[:ad].delete(:ad_days)
     @ad = Ad.new(params[:ad])
-    @ad.end_time = @ad.start_time + ad_days.to_i.days if ad_days
-    @ad.client_id = current_user.client_id
 
-    if @ad.save
-      redirect_to ads_path, :notice => 'Ad has been created and waiting for confirmation'
+    if @ad.valid?
+      @ad.end_time = @ad.start_time + ad_days.to_i.days if ad_days
+      @ad.client_id = current_user.client_id
+
+      if @ad.save
+        redirect_to ads_path, :notice => 'Ad has been created and waiting for confirmation'
+      else
+        render :new
+      end
     else
       render :new
     end
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -12,6 +12,8 @@ class Ad < ActiveRecord::Base
 
   before_save :set_price
 
+  validates :start_time, :presence => true, :format => { :with => /\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2}/ }
+
   def confirm!
     self[:confirmed] = true
     save!
