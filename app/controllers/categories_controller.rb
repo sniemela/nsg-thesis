7191a03ac235013a9296b629c664fc5e3bda6a30
Niklas Granö <niklas.grano@gmail.com>
2011-11-11 14:52:33 +0200

    Fix and add more breadcrumb paths

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,13 @@
 class CategoriesController < ApplicationController
   before_filter :admin_required, :except => [:index, :show]
+  add_breadcrumb I18n.t('title.categories'), :categories_path
   
   def index
     @categories = Category.all
-    add_breadcrumb I18n.t('title.categories'), :categories_path
   end
   
   def new
-    add_breadcrumb I18n.t('title.add_category'), :new_category_path    
+    add_breadcrumb I18n.t('title.add_category'), :new_category_path
     @category = Category.new
   end
   
@@ -25,10 +25,12 @@ class CategoriesController < ApplicationController
     @category = Category.find_by_name(params[:id])
     raise ActiveRecord::RecordNotFound unless @category
     @ads = Ad.find_by_tag_ids(@category.tags.select('tags.id'))
+    add_breadcrumb @category.name, category_path(@category)
   end
   
   def edit
     @category = Category.find_by_name(params[:id])
+    add_breadcrumb @category.name, category_path(@category)
   end
   
   def update
