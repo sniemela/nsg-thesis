bf7a0c3a5bc94522540a5b97733dbfe8b1a4e99f
Simo Niemelä <simo.niemela@gmail.com>
2011-11-20 11:40:11 +0200

    Add an address_and_city column to the event

--- db/migrate/20110813131120_create_events.rb
+++ db/migrate/20110813131120_create_events.rb
@@ -8,6 +8,7 @@ class CreateEvents < ActiveRecord::Migration
       t.string :country,            :null => false
       t.string :address
       t.string :city
+      t.string :address_and_city
       t.integer :times_watched,     :default => 0
       t.integer :liked_count,       :default => 0
       t.boolean :approved,          :default => false
